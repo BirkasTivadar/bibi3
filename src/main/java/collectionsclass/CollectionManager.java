e8f5e499f46149190a91bfc73b623676d3972d57
BirkasTivadar <birkastivadar@gmail.com>
2021-03-15 14:01:40 +0100

    Gyakorlati feladat - CollectionManager kész

---
+++ src/main/java/collectionsclass/CollectionManager.java
@@ -0,0 +1,32 @@
+package collectionsclass;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class CollectionManager {
+
+    private List<Book> library;
+
+    public CollectionManager(List<Book> library) {
+        this.library = library;
+    }
+
+    public List<Book> createUnmodifiableLibrary() {
+        return Collections.unmodifiableList(library);
+    }
+
+    public List<Book> reverseLibrary() {
+        List<Book> result = new ArrayList<>(library);
+        Collections.reverse(result);
+        return result;
+    }
+
+    public Book getFirstBook() {
+        return Collections.min(library);
+    }
+
+    public Book getLastBook() {
+        return Collections.max(library);
+    }
+}
